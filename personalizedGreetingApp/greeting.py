6b10900144696749f22fa6580c20211b5fde1211
Polymathh <wambugum110@gmail.com>
2024-12-19 09:27:10 +0300

    Personalized Greeting app

---
+++ personalizedGreetingApp/greeting.py
@@ -0,0 +1,7 @@
+# Personalized Greeting App 👋
+# Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+
+name = input('what\'s your name?')
+color = input('what\'s your favourite color?')
+
+print(f'Hello {name}! Your favourite color, {color} is awesome!!')
