97eeddf467b36079484a49d887d862145398d69e
Polymathh <wambugum110@gmail.com>
2025-01-27 21:51:57 +0300

    initial commit

--- cursive/tryin.py
+++ cursive/tryin.py
@@ -1,7 +1,28 @@
-def reverse_string(s):
-    return s[::-1]
+import turtle
+import colorsys
 
-# Example usage
-input_string = "Hello, World!"
-reversed_string = reverse_string(input_string)
-print(reversed_string)  # Output: !dlroW ,olleH
+def draw_colorful_spiral():
+    screen = turtle.Screen()
+    screen.bgcolor("black")
+    spiral = turtle.Turtle()
+    spiral.speed(0)
+    spiral.width(2)
+
+    # Total number of colors in the spiral
+    num_colors = 360
+    hue = 0  # Starting color hue
+
+    for i in range(num_colors * 2):
+        # Set the color using HSV to RGB conversion
+        color = colorsys.hsv_to_rgb(hue, 1, 1)
+        spiral.color(color)
+
+        # Draw the spiral
+        spiral.forward(i * 2)
+        spiral.right(59)
+        hue += 1 / num_colors  # Increment hue to change color gradually
+
+    spiral.hideturtle()
+    screen.mainloop()
+
+draw_colorful_spiral()
