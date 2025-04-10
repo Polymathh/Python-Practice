a2af7094d766c088738922d4bee4d5479245dbf3
Polymathh <wambugum110@gmail.com>
2025-04-10 10:26:50 +0300

    inital commit

---
+++ A_30DaysofPython/Day11/1_modules.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.sqrt(625))
+print(math.pi)
+
+
+#datetime Module
+
+import datetime
+today = datetime.date.today()
+print('Todays date is: ', today)
---
+++ A_30DaysofPython/Day11/3_import.py
@@ -0,0 +1,3 @@
+import my_module
+message = my_module.greet('Wambugu')
+print(message)
