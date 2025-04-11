060fa2899fc4e72a41351c1a44c38e272465d173
Polymathh <wambugum110@gmail.com>
2025-04-11 17:02:26 +0300

    inital commit

---
+++ A_30DaysofPython/Day12/repeat.py
@@ -0,0 +1,7 @@
+while True:
+    name = input("Enter your name (or 'exit' to quit): ")
+
+    if name == 'exit':
+        print('Goodbye!')
+        break
+    print(f"Hello, {name}! Welcome to day 12")
